38327e61d715684e095de56d02339546e773760d
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2023-11-08 15:54:37 +0300

    country code picker added

--- app/src/main/java/com/example/chatapp/ui/login/SendCodeFragment.kt
+++ app/src/main/java/com/example/chatapp/ui/login/SendCodeFragment.kt
@@ -2,11 +2,13 @@ package com.example.chatapp.ui.login
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.chatapp.R
 import com.example.chatapp.common.viewBinding
 import com.example.chatapp.databinding.FragmentSendCodeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class SendCodeFragment : Fragment(R.layout.fragment_send_code) {
@@ -18,8 +20,21 @@ class SendCodeFragment : Fragment(R.layout.fragment_send_code) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
-          
+            ccp.setDefaultCountryUsingNameCode(Locale.getDefault().country)
+            ccp.resetToDefaultCountry()
+
+            ccp.setOnCountryChangeListener {
+                val countryName = ccp.selectedCountryName
+                val countryCode = ccp.selectedCountryCode
+
+                Toast.makeText(
+                    requireContext(),
+                    "$countryCode,$countryName",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+
 
     }
 
